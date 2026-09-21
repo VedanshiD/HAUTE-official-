a89afd86c5b3a2bf9f1d789f8bfbe4a5256f3fa6
VedanshiD <pradhilearns@gmail.com>
2026-09-21 16:29:53 -0400

    menu popup
    finished menu popup, including stylizing stuff, and made it accesible from all pages

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 const popups = document.querySelectorAll(".popup");
 const popupimgs = document.querySelectorAll(".popupimg");
 const closebtn = document.querySelectorAll(".close");
-popupimgs.forEach((img,index) => {
-img.addEventListener("click", function() {
-    popups[index].style.display = "flex";
+if (popupimgs.length > 0) {
+    popupimgs.forEach((img,index) => {
+    img.addEventListener("click", function() {
+        popups[index].style.display = "flex";
+        });
     });
-});
-closebtn.forEach((btn,index) => {
-btn.addEventListener("click", function() {
-    popups[index].style.display = "none";
+    closebtn.forEach((btn,index) => {
+    btn.addEventListener("click", function() {
+        popups[index].style.display = "none";
+        });
     });
-});
+}
+const menu = document.getElementById("menu");
+const menuBtn = document.getElementById("menuBtn");
+const menuCloseBtn = document.getElementById("menuCloseBtn");
+
+if (menuBtn) {
+    menuBtn.addEventListener("click", function() {
+        menu.style.display = "flex";
+    });
+}
+if (menuCloseBtn) {
+    menuCloseBtn.addEventListener("click", function() {
+        menu.style.display = "none";
+    });
+}
